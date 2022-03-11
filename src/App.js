04e90d657442b94e428592428dc0df84a559f63c
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-03-11 15:29:52 +0530

    folder setup done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="font-body py-2 px-7">
+      <h1 className="text-5xl my-5 text-gray-900 font-body font-bold underline">Hello world!</h1>
+      <p className="text-2xl text-gray-800">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quos officia culpa ut sed blanditiis dolores et, aspernatur eos veniam reiciendis accusantium ab non dolore. Alias, perferendis obcaecati. Distinctio, ipsa.</p>
+    </div>
+  );
+}
+
+export default App;
