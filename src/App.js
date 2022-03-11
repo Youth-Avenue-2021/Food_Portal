faa14c24476ef11205c680b2c645b73cb0f69a4b
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-03-11 20:44:47 +0530

    pyboat added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import MessRanking from "./Components/MessRanking";
 import Navbar from "./Components/Navbar";
+import PyBoat from "./Components/PyBoat";
 import SearchSection from "./Components/SearchSection";
 
 function App() {
   return (
     <>
+      <PyBoat />
       <Navbar />
       <SearchSection />
       <MessRanking />
---
+++ src/Components/PyBoat.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PyBoat = () => {
+  return (
+    <>
+      <div className="fixed flex items-center justify-center w-16 h-16 rounded-full shadow-xl bottom-10 right-10 bg-sky-800">
+        <i className="text-2xl fa-solid fa-robot text-sky-100"></i>
+      </div>
+    </>
+  );
+};
+
+export default PyBoat;
