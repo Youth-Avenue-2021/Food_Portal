72f703778fada28cc46d9ad6f7545f32709217c5
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-03-12 09:09:36 +0530

    more links added in navbar

--- src/Components/MessCard.js
+++ src/Components/MessCard.js
@@ -8,11 +8,11 @@ const MessCard = () => {
         <div className="flex flex-col items-start justify-center p-5">
           <h2 className="py-2 text-2xl text-gray-900">Mess Name</h2>
           <div className="flex flex-row items-start justify-start mb-5">
-            <i class="text-sky-500 fa-solid fa-star-half-stroke"></i>
-            <i class="text-sky-500 fa-solid fa-star-half-stroke"></i>
-            <i class="text-sky-500 fa-solid fa-star-half-stroke"></i>
-            <i class="text-sky-500 fa-solid fa-star-half-stroke"></i>
-            <i class="text-sky-500 fa-solid fa-star-half-stroke"></i>
+            <i className="text-sky-500 fa-solid fa-star-half-stroke"></i>
+            <i className="text-sky-500 fa-solid fa-star-half-stroke"></i>
+            <i className="text-sky-500 fa-solid fa-star-half-stroke"></i>
+            <i className="text-sky-500 fa-solid fa-star-half-stroke"></i>
+            <i className="text-sky-500 fa-solid fa-star-half-stroke"></i>
           </div>
           <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque similique, temporibus eius sit impedit, </p>
           <a href="/mess" className="mt-3 underline text-sky-500">
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,19 +5,42 @@ const Navbar = () => {
         <div className="container flex items-center justify-between px-16 py-5 mx-auto">
           <h2 className="text-4xl font-bold text-sky-600">Food Portal</h2>
           <div className="flex flex-row items-center justify-center">
-            <a className="mx-2 text-2xl duration-200 hover:text-sky-500" href="https://youtube.com">
+            {/* <a className="mx-2 text-base duration-200 hover:text-sky-500" href="https://youtube.com">
               <i className="mr-1 text-sky-600 fa-solid fa-square-pen"></i>Write a complain
+            </a> */}
+            <a className="mx-2 text-base duration-200 hover:text-sky-500" href="https://youtube.com">
+              <i className="mr-1 text-sky-600 fa-solid fa-briefcase"></i> About
             </a>
-            <a className="mx-2 text-2xl duration-200 hover:text-sky-500" href="https://youtube.com">
-              About
+            <a className="mx-2 text-base duration-200 hover:text-sky-500" href="https://youtube.com">
+              <i className="mr-1 text-sky-600 fa-solid fa-star-of-life"></i>
+              Benefits
             </a>
-            <a className="mx-2 text-2xl duration-200 hover:text-sky-500" href="https://youtube.com">
+            <a className="mx-2 text-base duration-200 hover:text-sky-500" href="https://youtube.com">
+              <i className="mr-1 text-sky-600 fa-solid fa-rocket"></i>
+              Mission
+            </a>
+            <a className="mx-2 text-base duration-200 hover:text-sky-500" href="https://youtube.com">
+              <i className="mr-1 text-sky-600 fa-solid fa-briefcase"></i>
+              How do we work?
+            </a>
+            <a className="mx-2 text-base duration-200 hover:text-sky-500" href="https://youtube.com">
+              <i className="mr-1 text-sky-600 fa-solid fa-utensils"></i>
+              Food Standards
+            </a>
+            <a className="mx-2 text-base duration-200 hover:text-sky-500" href="https://youtube.com">
+              <i className="mr-1 text-sky-600 fa-solid fa-medal"></i>
+              Mess Ranking
+            </a>
+            <a className="mx-2 text-base duration-200 hover:text-sky-500" href="https://youtube.com">
               <i className="mr-1 text-sky-600 fa-solid fa-headset"></i>Contact
             </a>
           </div>
           <div className="flex items-center justify-center">
-            <a href="/login" className="px-4 py-2 text-white duration-200 rounded-md shadow-xl bg-sky-600 hover:text-sky-600 hover:bg-white">
+            <a href="/login" className="px-4 py-2 mx-2 text-white duration-200 rounded-md shadow-xl bg-sky-600 hover:text-sky-600 hover:bg-white">
               Login
+            </a>
+            <a href="/login" className="px-4 py-2 mx-2 text-white duration-200 rounded-md shadow-xl bg-sky-400 hover:text-sky-600 hover:bg-white">
+              Sign Up
             </a>
             <a href="/search" className="flex items-center justify-center w-10 h-10 mx-3 bg-gray-900 rounded-full shadow-md text-sky-500">
               <i className="fa-solid fa-magnifying-glass"></i>
